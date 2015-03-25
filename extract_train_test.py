8bed2d80020061654311a1f77e5d251abdd1c5d3
SnShine <shine123surya@gmail.com>
2015-03-25 10:12:38 +0530

    to train svm with a lot of features like all_fc7, 500_fc7, all_fc8, fc7_fc8

--- extract_train_test.py
+++ extract_train_test.py
@@ -1,13 +1,17 @@
+import time
+sttime= time.clock()
+print("Importing essential modules...")
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
-import time
+import sys
 
 caffe_root = "../../"  
 import sys
 sys.path.insert(0, caffe_root + "python")
 
 import caffe
+print("\t\tsuccessfully imported in "+str(time.clock()- sttime)+" Secs.\n")
 
 ####################
 ##   Parameters   ##
@@ -26,9 +30,6 @@ elif mynet== "googlenet":
     model= "models/bvlc_googlenet/bvlc_googlenet.caffemodel"
 else:
     print("Please give a proper net to extract!")
-
-#layer of CNN to extract
-mylayer= "fc7"         #accepted values are fc7, fc8, fc7+fc8
 
 
 #########################
@@ -55,15 +56,18 @@ transformer.set_raw_scale("data", 255)  # the reference model operates on images
 transformer.set_channel_swap("data", (2,1,0))  # the reference model has channels in BGR order instead of RGB
 net.blobs["data"].reshape(1,3,227,227)
 
-train_feats= []
+train_feats_fc7= []
+test_feats_fc7= []
 
-test_feats= []
+train_feats_fc8= []
+test_feats_fc8= []
+
+train_feats_fc7_fc8= []
+test_feats_fc7_fc8= []
 
 #####################
 ##   Load Labels   ##
 #####################
-
-sttime= time.clock()
 
 train_labels= []
 test_labels= []
@@ -72,20 +76,23 @@ print("Loading labels from files...")
 rdfile1= open("train_data.txt", "r")
 rdfile2= open("test_data.txt", "r")
 
+sttime= time.clock()
 #to load train_labels from file
 for line in rdfile1:
     line= line[:-1]
     train_labels.append(int(line.split()[1]))
 
 train_labels= np.array(train_labels)
+print("\t\t"+ str(len(train_labels))+" train_labels successfully loaded in "+str(time.clock()- sttime)+" Secs.\n")
 
+sttime= time.clock()
 #to load test labels from file
 for line in rdfile2:
     line= line[:-1]
     test_labels.append(int(line.split()[1]))
 
 test_labels= np.array(test_labels)
-print("\t\t"+ str(len(test_labels))+" labels successfully loaded in "+str(time.clock()- sttime)+" Secs.\n")
+print("\t\t"+ str(len(test_labels))+" test_labels successfully loaded in "+str(time.clock()- sttime)+" Secs.\n")
 
 
 ##########################
@@ -97,35 +104,30 @@ print("Extracting train features...")
 sttime= time.clock()
 train_file= open("train_data.txt","r")
 
-if mylayer== "fc7":
-    for line in train_file:
-        print(line)
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()   
-        feat= np.array(net.blobs["fc7"].data[0])
-        train_feats.append(feat) 
-    train_feats= np.array(train_feats)
-elif mylayer== "fc8":
-    for line in train_file:
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()
-        feat= np.array(net.blobs["fc8"].data[0])
-        train_feats.append(feat)
-    train_feats= np.array(train_feats)
-elif mylayer== "fc7+fc8":
-    for line in train_file:
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()
-        feat1= np.array(net.blobs["fc7"].data[0])
-        feat2= np.array(net.blobs["fc8"].data[0])
-        feat= np.append(feat1, feat2)
-        train_feats.append(feat)
-    train_feats= np.array(train_feats)
+total_train_images= str(len(train_labels))
+current_image= 1
+for line in train_file:
+    temp_print= (str(current_image)+ "/"+ total_train_images+ "... ")
+    sys.stdout.write(temp_print)
+    sys.stdout.flush()
+    current_image+= 1
 
-print("\t\tsuccessfully extracted in "+str(time.clock()- sttime)+" Secs.")
+    line1= line.split()
+    net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
+    out= net.forward()   
+
+    feat_fc7= np.array(net.blobs["fc7"].data[0])
+    feat_fc8= np.array(net.blobs["fc8"].data[0])
+    feat_fc7_fc8= np.append(feat_fc7, feat_fc8)
+    train_feats_fc7.append(feat_fc7)
+    train_feats_fc8.append(feat_fc8)
+    train_feats_fc7_fc8.append(feat_fc7_fc8)
+    
+train_feats_fc7= np.array(train_feats_fc7)
+train_feats_fc8= np.array(train_feats_fc8)
+train_feats_fc7_fc8= np.array(train_feats_fc7_fc8) 
+
+print("\n\t\tsuccessfully extracted in "+str(time.clock()- sttime)+" Secs.")
 
 
 #test features
@@ -133,71 +135,67 @@ print("Extracting test features...")
 sttime= time.clock()
 test_file= open("test_data.txt", "r")
 
-if mylayer== "fc7":
-    for line in test_file:
-        print(line)
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()   
-        feat= np.array(net.blobs["fc7"].data[0])
-        test_feats.append(feat) 
-    test_feats= np.array(test_feats)
-elif mylayer== "fc8":
-    for line in train_file:
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()
-        feat= np.array(net.blobs["fc8"].data[0])
-        test_feats.append(feat) 
-    test_feats= np.array(test_feats)
-elif mylayer== "fc7+fc8":
-    for line in train_file:
-        line1= line.split()
-        net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
-        out= net.forward()
-        feat1= np.array(net.blobs["fc7"].data[0])
-        feat2= np.array(net.blobs["fc8"].data[0])
-        feat= np.append(feat1, feat2)
-        test_feats.append(feat) 
-    test_feats= np.array(test_feats)
+total_test_images= str(len(test_labels))
+current_image= 1
+for line in test_file:
+    temp_print= (str(current_image)+ "/"+ total_test_images+ "... ")
+    sys.stdout.write(temp_print)
+    sys.stdout.flush()
+    current_image+= 1
 
-print("\t\tsuccessfully extracted in "+str(time.clock()- sttime)+" Secs.")
+    line1= line.split()
+    net.blobs["data"].data[...]= transformer.preprocess("data", caffe.io.load_image(line1[0]))
+    out= net.forward()   
+
+    feat_fc7= np.array(net.blobs["fc7"].data[0])
+    feat_fc8= np.array(net.blobs["fc8"].data[0])
+    feat_fc7_fc8= np.append(feat_fc7, feat_fc8)
+    test_feats_fc7.append(feat_fc7)
+    test_feats_fc8.append(feat_fc8)
+    test_feats_fc7_fc8.append(feat_fc7_fc8)
+    
+test_feats_fc7= np.array(test_feats_fc7)
+test_feats_fc8= np.array(test_feats_fc8)
+test_feats_fc7_fc8= np.array(test_feats_fc7_fc8) 
+
+print("\n\t\tsuccessfully extracted in "+str(time.clock()- sttime)+" Secs.")
 
 
 ######################
 ##   SVM Training   ##
 ######################
 
-print("Starting to classify with svm")
+
+#1st svm fit
+print("Svm Classification with first 500 features of layer fc7")
 C=1.0
 
 print("\tfitting svc...")
 sttime= time.clock()
-svc = svm.SVC(kernel='linear', C=C).fit(train_feats[:,:500], train_labels)
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc7[:,:500], train_labels)
 print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
 
 print("\tfitting rbf_svc...")
 sttime= time.clock()
-rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats[:,:500], train_labels)
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc7[:,:500], train_labels)
 print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
 
 print("\tfitting poly_svc...")
 sttime= time.clock()
-poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats[:,:500], train_labels)
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc7[:,:500], train_labels)
 print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
 
 print("\tfitting lin_svc...")
 sttime= time.clock()
-lin_svc = svm.LinearSVC(C=C).fit(train_feats[:,:500], train_labels)
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc7[:,:500], train_labels)
 print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
-
 
 print("\n\nPredicting values...")
 for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
     sttime= time.clock()
     print("\nClassifier: ")
     print(clf)
-    Z = clf.predict(test_feats[:,:500])
+    Z = clf.predict(test_feats_fc7[:,:500])
     #print(Z)
     #print(test_labels)
     scores = (Z== test_labels)
@@ -205,4 +203,204 @@ for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
     print("Accuracy: ")
     print("\t\t"+str(total/500.0))
     print("Time: ")
-    print("\t\t"+ str(time.clock()- sttime)+ " Secs.")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
+
+
+#2nd svm fit
+print("Svm Classification with first half (2048) features of layer fc7")
+C=1.0
+
+print("\tfitting svc...")
+sttime= time.clock()
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc7[:,:2048], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting rbf_svc...")
+sttime= time.clock()
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc7[:,:2048], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting poly_svc...")
+sttime= time.clock()
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc7[:,:2048], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting lin_svc...")
+sttime= time.clock()
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc7[:,:2048], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\n\nPredicting values...")
+for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
+    sttime= time.clock()
+    print("\nClassifier: ")
+    print(clf)
+    Z = clf.predict(test_feats_fc7[:,:2048])
+    #print(Z)
+    #print(test_labels)
+    scores = (Z== test_labels)
+    total = np.sum(scores)
+    print("Accuracy: ")
+    print("\t\t"+str(total/500.0))
+    print("Time: ")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
+
+
+#3rd svm fit
+print("Svm Classification with all the features of layer fc7")
+C=1.0
+
+print("\tfitting svc...")
+sttime= time.clock()
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc7, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting rbf_svc...")
+sttime= time.clock()
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc7, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting poly_svc...")
+sttime= time.clock()
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc7, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting lin_svc...")
+sttime= time.clock()
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc7, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\n\nPredicting values...")
+for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
+    sttime= time.clock()
+    print("\nClassifier: ")
+    print(clf)
+    Z = clf.predict(test_feats_fc7)
+    #print(Z)
+    #print(test_labels)
+    scores = (Z== test_labels)
+    total = np.sum(scores)
+    print("Accuracy: ")
+    print("\t\t"+str(total/500.0))
+    print("Time: ")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
+
+
+#4th svm fit
+print("Svm Classification with first half (500) features of layer fc8")
+C=1.0
+
+print("\tfitting svc...")
+sttime= time.clock()
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc8[:,:500], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting rbf_svc...")
+sttime= time.clock()
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc8[:,:500], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting poly_svc...")
+sttime= time.clock()
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc8[:,:500], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting lin_svc...")
+sttime= time.clock()
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc8[:,:500], train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\n\nPredicting values...")
+for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
+    sttime= time.clock()
+    print("\nClassifier: ")
+    print(clf)
+    Z = clf.predict(test_feats_fc8[:,:500])
+    #print(Z)
+    #print(test_labels)
+    scores = (Z== test_labels)
+    total = np.sum(scores)
+    print("Accuracy: ")
+    print("\t\t"+str(total/500.0))
+    print("Time: ")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
+
+
+#5th svm fit
+print("Svm Classification with all the features of layer fc8")
+C=1.0
+
+print("\tfitting svc...")
+sttime= time.clock()
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting rbf_svc...")
+sttime= time.clock()
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting poly_svc...")
+sttime= time.clock()
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting lin_svc...")
+sttime= time.clock()
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\n\nPredicting values...")
+for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
+    sttime= time.clock()
+    print("\nClassifier: ")
+    print(clf)
+    Z = clf.predict(test_feats_fc8)
+    #print(Z)
+    #print(test_labels)
+    scores = (Z== test_labels)
+    total = np.sum(scores)
+    print("Accuracy: ")
+    print("\t\t"+str(total/500.0))
+    print("Time: ")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
+
+
+#6th svm fit
+print("Svm Classification with all the features of layer fc7_fc8")
+C=1.0
+
+print("\tfitting svc...")
+sttime= time.clock()
+svc = svm.SVC(kernel='linear', C=C).fit(train_feats_fc7_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting rbf_svc...")
+sttime= time.clock()
+rbf_svc = svm.SVC(kernel='rbf', gamma=0.7, C=C).fit(train_feats_fc7_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting poly_svc...")
+sttime= time.clock()
+poly_svc = svm.SVC(kernel='poly', degree=3, C=C).fit(train_feats_fc7_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\tfitting lin_svc...")
+sttime= time.clock()
+lin_svc = svm.LinearSVC(C=C).fit(train_feats_fc7_fc8, train_labels)
+print("\t\ttook "+str(time.clock()- sttime)+ " Secs.")
+
+print("\n\nPredicting values...")
+for i, clf in enumerate((svc, rbf_svc, poly_svc, lin_svc)):
+    sttime= time.clock()
+    print("\nClassifier: ")
+    print(clf)
+    Z = clf.predict(test_feats_fc7_fc8)
+    #print(Z)
+    #print(test_labels)
+    scores = (Z== test_labels)
+    total = np.sum(scores)
+    print("Accuracy: ")
+    print("\t\t"+str(total/500.0))
+    print("Time: ")
+    print("\t\t"+ str(time.clock()- sttime)+ " Secs.\n")
